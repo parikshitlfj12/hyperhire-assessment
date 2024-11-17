fa5ae8d2e73ad859ca399cb927858e18305f30d2
Parikshit Personal <parikshitlfj@gmail.com>
2024-11-17 19:48:49 +0530

    minor: fetching fixes

--- src/app/api/data/route.ts
+++ src/app/api/data/route.ts
@@ -8,19 +8,24 @@ import {
 } from "@/utils/constants";
 
 export async function GET() {
-  const data = {
-    bannerSliderItems: BANNER_SLIDER_ITEMS,
-    carousalProfiles: CAROUSAL_PROFILES,
-    descriptionAbout: DESCRIPTION_ABOUT,
-    descriptionItems: DESCRIPTION_ITEMS,
-    dropDownOptions: DROPDOWN_OPTIONS,
-  };
-
   try {
+    const data = {
+      bannerSliderItems: BANNER_SLIDER_ITEMS,
+      carousalProfiles: CAROUSAL_PROFILES,
+      descriptionAbout: DESCRIPTION_ABOUT,
+      descriptionItems: DESCRIPTION_ITEMS,
+      dropDownOptions: DROPDOWN_OPTIONS,
+    };
     const jsonData = JSON.stringify(data);
-    return new NextResponse(jsonData, { status: 200, headers: { 'Content-Type': 'application/json' } });
+    return new NextResponse(jsonData, {
+      status: 200,
+      headers: { "Content-Type": "application/json" },
+    });
   } catch (error) {
     console.error("Error serializing response:", error);
-    return new NextResponse(JSON.stringify({ error: "Internal Server Error" }), { status: 500 });
+    return new NextResponse(
+      JSON.stringify({ error: "Internal Server Error" }),
+      { status: 500 }
+    );
   }
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,16 +2,23 @@ import HomePageTemplate from "@/components/templates/HomePageTemplate";
 
 export default async function Page() {
   const apiBaseUrl = process.env.NEXT_PUBLIC_API_BASE_URL;
-  const res = await fetch(`${apiBaseUrl}/api/data`);
-  const data = await res.json();
-
-  return (
-    <HomePageTemplate
-      bannerSliderItems={data.bannerSliderItems}
-      carousalProfiles={data.carousalProfiles}
-      descriptionAbout={data.descriptionAbout}
-      descriptionItems={data.descriptionItems}
-      dropDownOptions={data.dropDownOptions}
-    />
-  );
+  try {
+    const res = await fetch(`${apiBaseUrl}/api/data`);
+    if (!res.ok) {
+      throw new Error(`HTTP error! Status: ${res.status}`);
+    }
+    const data = await res.json();
+    return (
+      <HomePageTemplate
+        bannerSliderItems={data.bannerSliderItems}
+        carousalProfiles={data.carousalProfiles}
+        descriptionAbout={data.descriptionAbout}
+        descriptionItems={data.descriptionItems}
+        dropDownOptions={data.dropDownOptions}
+      />
+    );
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    return <div>Error fetching data. Please try again later.</div>;
+  }
 }
